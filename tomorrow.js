99806a3c334cba594ffda418b86ff957bb143f5a
Yunoxa <58912199+Yunoxa@users.noreply.github.com>
2020-01-08 21:24:49 +0000

    Update tomorrow.js

--- tomorrow.js
+++ tomorrow.js
@@ -1,7 +1,7 @@
 module.exports = {
 	name: 'tomorrow',
 	description: 'If I can do it tomorrow, I wont do it today!',
-	execute(message, args) {
+	execute(message) {
 		message.channel.send('https://cdn.discordapp.com/attachments/569577071911632948/655885856908902400/Procrastination.gif');
 	},
 };
