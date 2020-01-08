d8d10812e37bea56c8ba587493e96b3f9e531d36
Yunoxa <58912199+Yunoxa@users.noreply.github.com>
2020-01-08 21:35:07 +0000

    Update yui.js

--- yui.js
+++ yui.js
@@ -1,12 +1,11 @@
-number = 112;
+number = 114;
 module.exports = {
     name: 'yui',
 description: `Send an image featuring the greatest girl, Yui Funami. Currently ${number} unique images available.`,
 guildOnly: false,
 cooldown: 1,
 execute(message) {
-    
-    number = 112,
+       
     imageNumber = Math.floor (Math.random() * number) + 1;
     message.channel.send ({files: ["./ImagesYui/" + imageNumber + ".png"]})
         
