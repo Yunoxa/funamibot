5e31ad9150267e5ce0fc4ed23c5b19a439f9a4bc
Yunoxa <58912199+Yunoxa@users.noreply.github.com>
2020-01-10 22:59:00 +0000

    Delete changelog.js

--- changelog.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'changelog',
-	description: 'Sends a text file which has update information',
-	execute(message) {
-		message.channel.send ({files: ["ChangeLog.txt"]})
-	},
-};
