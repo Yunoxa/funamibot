164c37e221c25ad4faf2ad46cbb14abf3887b745
Yunoxa <58912199+Yunoxa@users.noreply.github.com>
2020-01-10 22:59:59 +0000

    Delete tomorrow.js

--- tomorrow.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'tomorrow',
-	description: 'If I can do it tomorrow, I wont do it today!',
-	execute(message) {
-		message.channel.send('https://cdn.discordapp.com/attachments/569577071911632948/655885856908902400/Procrastination.gif');
-	},
-};
